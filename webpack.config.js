7cd26408219bf06bb35a18f10f8c4201aabceff5
Will Amaral <willian.amaral@outlook.com.br>
2018-09-22 04:55:03 -0300

    Created boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	entry: './client/src/index.js',
+	module: {
+		rules: [
+			{test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'},
+			{
+				test: /\.(sass|css|scss)$/, 
+				use: [
+					process.env.NODE_ENV == 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+					'sass-loader',
+					'css-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new HTMLWebpackPlugin({template: './client/public/index.html'}),
+		new MiniCssExtractPlugin()
+	]
+};
