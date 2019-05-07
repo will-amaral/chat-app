753d0dba2e405a48185446a7a47a22bc08e3f40f
Willian Amaral <willian.amaral@outlook.com.br>
2019-05-07 00:28:16 -0300

    Added server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+const numUsers = 0;
+
+app.get('/', async (req, res) => {
+    res.send({ response: 'I am alive'}).status(200);
+  });
+
+io.on('connection', socket => {
+    numUsers++;
+    console.log('usuário conectado! Total de usuários: ' + numUsers);
+    socket.on('disconnect', () => {
+        numUsers--;
+        console.log('usuário desconectado');
+    })
+});
+
+server.listen(8081, () =>{
+    console.log('🚀 Servidor online em http://localhost:8081')
+});
